1834709d8a95630fdbcdd41d5723816701fea593
classabbyamp <dev@kb6.ee>
2022-03-19 23:01:53 -0400

    add 3.9 and 3.10 to setup.py tags

--- setup.py
+++ setup.py
@@ -24,6 +24,8 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: Developers",
